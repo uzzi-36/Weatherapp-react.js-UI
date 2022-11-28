b85d8ca369197d7829c6612fa1242c4a987b1d32
uzzi-36 <uzairsuper69@gmail.com>
2022-11-28 21:16:28 +0500

    123
    123

---
+++ weatherapp-ui/src/weatherservice.js
@@ -0,0 +1,36 @@
+const API_KEY = "f318046f0a17c9be9a49350919a17864"
+
+
+const getFormattedWeatherData = async (city, units = "metric") => {
+    const URL = 'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=${API_KEY}&units=${units}';
+
+    const data = await fetch(URL)
+        .then((res) => res.json())
+        .then((data) => data);
+
+    const {
+        weather,
+        main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+        wind: { speed },
+        sys: { country },
+        name,
+    } = data;
+
+    const { description, icon } = weather[0];
+
+    return {
+        description,
+        iconURL: makeIconURL(icon),
+        temp,
+        feels_like,
+        temp_min,
+        temp_max,
+        pressure,
+        humidity,
+        speed,
+        country,
+        name,
+    };
+
+};
+export { getFormattedWeatherData }; 
